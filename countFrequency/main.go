cb5c76fe6904f42ef6f37ccf96d21cbd2d4452d7
Dshuishui <dyucong@email.ncu.edu.cn>
2025-10-25 13:14:23 +0800

    add a use case of counting the frequency of each character in a string by using a map

---
+++ countFrequency/main.go
@@ -0,0 +1,23 @@
+// Use map to count the frequency of each character in a string and return the most frequent character.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+	mapS := make(map[byte]int)
+	maxCount := 0
+	var maxChar byte
+	for i := 0; i < len(s); i++ {
+		mapS[s[i]]++
+		if mapS[s[i]] > maxCount {
+			maxCount = mapS[s[i]]
+			maxChar = s[i]
+		}
+	}
+	fmt.Printf("%c\n",maxChar)
+}
